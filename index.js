ba1402a526760686706d3ce4281b0da7fc45c286
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-06-26 13:58:55 -0700

    inital signup command

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 #!/usr/bin/env node --use_strict
 
 const program = require('commander');
-const request = rquire('superagent');
+const superagent = require('superagent-use');
+const superPromise = require('superagent-promise-plugin');
+const chalk = require('chalk');
+
+superagent.use(superPromise);
+
+const signup = function(user, pass) {
+  const url = process.env.BREW_BUDDY_REMOTE;
+  return new Promise((resolve, reject) => {
+    superagent
+      .post(url + 'api/signup')
+      .set('Content-Type', 'application/json')
+      .send({username: user, password: pass})
+      .then((res) => {
+        process.env.BREW_BUDDY_USER_TOKEN = res.text;
+        resolve();})
+      .catch(reject);
+  });
+};
+
 program
-  .arguments('<file>')
   .option('-u, --username <username>', 'The user to authenticate as')
-  .option('-p, --password <password>', 'The user\'s password')
-  .action(function(file) {
-    console.log('%s is trying to authenticate with %s', program.username, program.password);
-  })
-  .parse(process.argv);
+  .option('-p, --password <password>', 'The user\'s password');
+
+program
+  .command('signup')
+  .description('signup for the BrewBuddy API')
+  .action(function() {
+    signup(program.username, program.password)
+    .then(() => console.log(chalk.green('You signed up successfully') ))
+    .catch((err) => console.log(chalk.red('there was an error', err.error.text)));
+  });
+program.parse(process.argv);
