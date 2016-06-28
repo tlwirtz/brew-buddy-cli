d0240ea2e9228864014410a61c5ddf4b48d0f80c
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-06-27 19:04:59 -0700

    now pulling origins and flavors

--- index.js
+++ index.js
@@ -69,6 +69,50 @@ const getAllEntries = function() {
   });
 };
 
+const getAllFlavors = function() {
+  const url = process.env.BREW_BUDDY_REMOTE;
+  const token = getToken();
+  return new Promise((resolve, reject) => {
+    superagent
+      .get(url + 'api/flavor/all')
+      .set('Content-Type', 'application-json')
+      .set('Authorization', 'Bearer ' + token)
+      .then((res) => {
+        resolve(res.text);
+      })
+      .catch(reject);
+  });
+};
+
+const getAllOrigins = function() {
+  const url = process.env.BREW_BUDDY_REMOTE;
+  const token = getToken();
+  return new Promise((resolve, reject) => {
+    superagent
+      .get(url + 'api/origin/all')
+      .set('Content-Type', 'application-json')
+      .set('Authorization', 'Bearer ' + token)
+      .then((res) => {
+        resolve(res.text);
+      })
+      .catch(reject);
+  });
+};
+
+const formatOutput = function(entries) {
+  const formatted = [];
+  entries.map((entry) => {
+    let line = '';
+    Object.keys(entry).map((key) => line += `${chalk.blue(key.toUpperCase())} ::  ${entry[key]}\n`);
+    formatted.push(line);
+  });
+
+  formatted.map((format) => {
+    console.log(format);
+  });
+  return formatted;
+};
+
 program
   .option('-u, --username <username>', 'The user to authenticate as')
   .option('-p, --password <password>', 'The user\'s password');
@@ -87,9 +131,33 @@ program
   .description('get all the entires')
   .action(function() {
     getAllEntries()
-    .then((entries) => console.log(chalk.green('entres:: ' + entries)))
+    // .then((entries) => console.log(chalk.green('entres:: ' + entries)))
+    .then((entries) => formatOutput(JSON.parse(entries)))
+    .then()
     .catch((err) => console.log(chalk.red('error' + err.error.text)));
   });
+
+program
+    .command('get-flavors')
+    .description('get all the flavors')
+    .action(function() {
+      getAllFlavors()
+      // .then((entries) => console.log(chalk.green('entres:: ' + entries)))
+      .then((flavors) => formatOutput(JSON.parse(flavors)))
+      .then()
+      .catch((err) => console.log(chalk.red('error' + err.error.text)));
+    });
+
+program
+    .command('get-origins')
+    .description('get all the origins')
+    .action(function() {
+      getAllOrigins()
+      // .then((entries) => console.log(chalk.green('entres:: ' + entries)))
+      .then((origins) => formatOutput(JSON.parse(origins)))
+      .then()
+      .catch((err) => console.log(chalk.red('error' + err.error.text)));
+    });
 
 program
   .command('signin')
